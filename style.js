1e0a1ad3d33b79fecaf1aca3edb5bb8556defe0a
Riccardo <riky.volto@live.com>
2023-09-20 14:48:35 +0200

    find random mail with log

--- style.js
+++ style.js
@@ -17,5 +17,12 @@ const { createApp } = Vue
     data() {
       return {
       }
+    },
+
+    mounted() {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(response => {
+            console.log(response.data.response);
+        })
     }
   }).mount('#app')
